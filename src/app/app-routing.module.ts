8269e7fa2518b6d0b87ec8e1547d732c38bd97a2
Sungchan <quijanojamesv@gmail.com>
2025-07-13 16:56:22 +0800

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./features/auth/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'assessment',
+    loadChildren: () => import('./features/assessment/assessment/assessment.module').then( m => m.AssessmentPageModule)
   }
 ];
 @NgModule({
