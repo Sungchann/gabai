521ee2d4bd8d53fb98a587d24cc2452c030d4134
Emyeeeel <maryamielcatado@gmail.com>
2025-07-14 21:19:04 +0800

    splash screen to home flow created

--- src/app/shared/component/loading-screen/loading-screen.component.ts
+++ src/app/shared/component/loading-screen/loading-screen.component.ts
@@ -1,13 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loading-screen',
   templateUrl: './loading-screen.component.html',
   styleUrls: ['./loading-screen.component.scss'],
+  standalone: true,
+  imports: [] // Add required imports if this is a standalone component
 })
 export class LoadingScreenComponent implements OnInit {
+  @Input() redirectDelay: number = 2000; // Default to 2 seconds
+  @Input() redirectPath: string = '/tabs'; // Default to tabs path
 
-  constructor() {}
+  constructor(private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // Set timeout to navigate after specified delay
+    setTimeout(() => {
+      this.router.navigateByUrl(this.redirectPath);
+    }, this.redirectDelay);
+  }
 }
